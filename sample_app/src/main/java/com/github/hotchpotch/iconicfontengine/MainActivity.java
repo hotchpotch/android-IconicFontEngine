b4109cb9fc73002915f33b21283cf1c79718df61
Yuichi Tateno <hotchpotch@gmail.com>
2015-03-06 17:40:55 +0900

    Change sample app

--- sample_app/src/main/java/com/github/hotchpotch/iconicfontengine/MainActivity.java
+++ sample_app/src/main/java/com/github/hotchpotch/iconicfontengine/MainActivity.java
@@ -9,14 +9,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
 
 import com.github.hotchpotch.fontengine.IconicFontEngine;
-import com.github.hotchpotch.fontengine.widget.IconicFontTextView;
 
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
 
@@ -133,50 +127,4 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         }
     }
 
-    public static class MainFragment extends Fragment {
-        public static MainFragment newInstance() {
-            return new MainFragment();
-        }
-        public MainFragment() {}
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_main, container, false);
-        }
-    }
-
-    public static class IconicFontEngineIconsFragment extends Fragment {
-        private static final String ENGINE_POSITION = "engine_position";
-
-        public static IconicFontEngineIconsFragment newInstance(int position) {
-            IconicFontEngineIconsFragment fragment = new IconicFontEngineIconsFragment();
-            Bundle args = new Bundle();
-            args.putInt(ENGINE_POSITION, position);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public IconicFontEngineIconsFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            ScrollView rootView = (ScrollView) inflater.inflate(R.layout.iconic_font_engine_icons_fragment, container, false);
-            LinearLayout iconicContainer = (LinearLayout) rootView.findViewById(R.id.iconic_container);
-            int enginePosition = getArguments().getInt(ENGINE_POSITION);
-            IconicFontEngine engine = IconicFontEngine.getDefaultEngines().get(enginePosition);
-            int position = 0;
-            for (String iconName: engine.getIconicFontMap().list()) {
-                LinearLayout view = (LinearLayout) inflater.inflate(R.layout.iconic_view_layout, container, false);
-                IconicFontTextView tv = (IconicFontTextView) view.findViewById(R.id.iconic_font_text_view);
-                tv.setText("{" + iconName + "} " + iconName);
-                view.setBackgroundResource(position % 2 == 0 ? R.color.background_even : R.color.background_odd);
-                iconicContainer.addView(view);
-                position++;
-            }
-            return rootView;
-        }
-    }
 }
---
+++ sample_app/src/main/java/com/github/hotchpotch/iconicfontengine/MainFragment.java
@@ -0,0 +1,30 @@
+package com.github.hotchpotch.iconicfontengine;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.github.hotchpotch.fontengine.widget.IconicFontTextView;
+
+/**
+* Created by yuichi-tateno on 2015/03/06.
+*/
+public class MainFragment extends Fragment {
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+    public MainFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        LinearLayout rootView = (LinearLayout) inflater.inflate(R.layout.fragment_main, container, false);
+        IconicFontTextView textView = (IconicFontTextView) rootView.findViewById(R.id.iconic_font_text_view_html);
+        textView.setText(Html.fromHtml("html example: <font color=#FF0000>{fa-github}</font><font color=#00FF00>{fa-github}</font><font color=#0000FF>{fa-github}</font>"));
+        return rootView;
+    }
+}
